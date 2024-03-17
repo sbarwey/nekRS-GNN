969be7cc6a0b069e8c2aa08ad53d947f46bd0de9
Shivam <sbarwey@polaris-login-01.hsn.cm.polaris.alcf.anl.gov>
2024-03-17 23:58:40 +0000

    SB: main update

--- 3rd_party/gnn/main.py
+++ 3rd_party/gnn/main.py
@@ -540,8 +540,8 @@ class Trainer:
 
         # Load data 
         main_path = self.cfg.gnn_outputs_path
-        path_to_x = main_path + 'fld_u_time_10.0_rank_%d_size_%d' %(RANK,SIZE)
-        path_to_y = main_path + 'fld_u_time_10.0_rank_%d_size_%d' %(RANK,SIZE)
+        path_to_x = main_path + 'fld_u_time_0.0_rank_%d_size_%d' %(RANK,SIZE)
+        path_to_y = main_path + 'fld_u_time_0.0_rank_%d_size_%d' %(RANK,SIZE)
         data_x = np.fromfile(path_to_x + ".bin", dtype=np.float64).reshape((-1,3))
         data_x = data_x.astype(NP_FLOAT_DTYPE)
         data_y = np.fromfile(path_to_y + ".bin", dtype=np.float64).reshape((-1,3))
